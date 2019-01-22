471084cca242a6577bf828eef555a0da19a825e4
zyongqing <zyongqing@gmail.com>
2019-01-22 15:45:33 +0800

    add debug

--- flask.py
+++ flask.py
@@ -2,8 +2,8 @@ from werkzeug.wrappers import Request, Response
 
 
 class Flask(object):
-    def __init__(self, **options):
-        pass
+    def __init__(self):
+        self.debug = False
 
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
@@ -11,12 +11,18 @@ class Flask(object):
                             mimetype='text/plain')
         return response(environ, start_response)
 
+    def run(self, host='localhost', port=5000, **options):
+        from werkzeug.serving import run_simple
+        if 'debug' in options:
+            self.debug = options.pop('debug')
+        options.setdefault('use_reloader', self.debug)
+        options.setdefault('use_debugger', self.debug)
+        run_simple(host, port, self, **options)
+
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
 
 
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-
     app = Flask()
-    run_simple('0.0.0.0', 8000, app, use_reloader=True, use_debugger=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
