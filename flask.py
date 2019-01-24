a25d83ababc3e314e794da4ce82aba7a836ab525
zyongqing <zyongqing@gmail.com>
2019-01-24 13:25:08 +0800

    add blueprint

--- flask.py
+++ flask.py
@@ -32,6 +32,7 @@ class Flask(object):
         self.name = name
         self.url_map = Map()
         self.view_functions = {}
+        self.blueprints = {}
 
     def dispatch_request(self):
         endpoint, values = _request_ctx_stack.top.url_adapter.match()
@@ -52,12 +53,19 @@ class Flask(object):
         if session is not None:
             session.save_cookie(response, key=self.session_cookie_name)
 
+    def add_url_rule(self, rule, endpoint, view_func, **options):
+        options['endpoint'] = endpoint
+        self.url_map.add(Rule(rule, **options))
+        self.view_functions[options['endpoint']] = view_func
+
+    def register_blueprint(self, blueprint, **options):
+        self.blueprints[blueprint.name] = blueprint
+        blueprint.register(self, **options)
+
     def route(self, rule, **options):
         def decorator(f):
-            if 'endpoint' not in options:
-                options['endpoint'] = f.__name__
-            self.url_map.add(Rule(rule, **options))
-            self.view_functions[options['endpoint']] = f
+            endpoint = options.get("endpoint", f.__name__)
+            self.add_url_rule(rule, endpoint, f, **options)
             return f
         return decorator
 
@@ -87,12 +95,59 @@ _request_ctx_stack = LocalStack()
 current_app = LocalProxy(lambda: _request_ctx_stack.top.app)
 request = LocalProxy(lambda: _request_ctx_stack.top.request)
 
+
+class BlueprintSetupState(object):
+    def __init__(self, blueprint, app, **options):
+        self.blueprint = blueprint
+        self.app = app
+        self.url_prefix = options.get('url_prefix',
+                                      '/%s' % self.blueprint.name)
+
+    def add_url_rule(self, rule, endpoint, view_func, **options):
+        rule = '/'.join((self.url_prefix, rule.lstrip('/')))
+        endpoint = '%s.%s' % (self.blueprint.name, endpoint)
+        self.app.add_url_rule(rule, endpoint, view_func, **options)
+
+
+class Blueprint(object):
+    def __init__(self, name):
+        self.name = name
+        self.deferred_functions = []
+
+    def register(self, app, **options):
+        state = BlueprintSetupState(self, app, **options)
+        for deferred in self.deferred_functions:
+            deferred(state)
+
+    def record(self, func):
+        self.deferred_functions.append(func)
+
+    def add_url_rule(self, rule, endpoint, view_func, **options):
+        self.record(lambda s: s.add_url_rule(rule, endpoint, view_func,
+                                             **options))
+
+    def route(self, rule, **options):
+        def decorator(f):
+            endpoint = options.get("endpoint", f.__name__)
+            self.add_url_rule(rule, endpoint, f, **options)
+            return f
+        return decorator
+
+
 if __name__ == '__main__':
+    bp = Blueprint('foo')
+
+    @bp.route('/hello/<name>', methods=['GET'])
+    def foo_hello(name):
+        return 'foo hello %s' % name
+
     app = Flask(__name__)
     app.secret_key = 'this_is_secret_key'
 
     @app.route('/hello/<name>', methods=['GET'])
     def hello(name):
         return 'hello %s' % name
+
+    app.register_blueprint(bp)
 
     app.run(host='0.0.0.0', port=8000, debug=True)
